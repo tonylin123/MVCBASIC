10b24f4cf809b3c513658b0b75b4b48c41f11aa0
Elev <Elev@DESKTOP-QA297QM>
2022-11-14 10:27:41 +0100

    Assignment 1

--- MVCBasic/Controllers/HomeController.cs
+++ MVCBasic/Controllers/HomeController.cs
@@ -18,11 +18,18 @@ namespace MVCBasic.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
-
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Projects()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
